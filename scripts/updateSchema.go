dd91fc64823ba1ccb4fed6d3cb160423c898c55e
Benjamin Sidelinger <ben.sidelinger@gmail.com>
2016-01-01 11:38:43 -0800

    fixed update schema and changed schema/data to match tutorial

--- scripts/updateSchema.go
+++ scripts/updateSchema.go
@@ -2,34 +2,36 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/graphql/testutil"
-	"github.com/sogko/golang-relay-starter-kit/data"
 	"io/ioutil"
 	"log"
 	"os"
+
+	"github.com/bsidelinger912/golang-relay-starter-kit/data"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/testutil"
 )
 
 func main() {
 	// Save JSON of full schema introspection for Babel Relay Plugin to use
-	result := graphql.Graphql(graphql.Params{
+	result := graphql.Do(graphql.Params{
 		Schema:        data.Schema,
 		RequestString: testutil.IntrospectionQuery,
 	})
+
 	if result.HasErrors() {
 		log.Fatalf("ERROR introspecting schema: %v", result.Errors)
 		return
-	} else {
-		b, err := json.MarshalIndent(result, "", "  ")
-		if err != nil {
-			log.Fatalf("ERROR: %v", err)
-		}
-		err = ioutil.WriteFile("../data/schema.json", b, os.ModePerm)
-		if err != nil {
-			log.Fatalf("ERROR: %v", err)
-		}
-
 	}
+
+	b, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+	err = ioutil.WriteFile("../data/schema.json", b, os.ModePerm)
+	if err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+
 	// TODO: Save user readable type system shorthand of schema
 	// pending implementation of printSchema
 	/*
